c9202335cf2615adc0ce0a673f5d1e0b800d5d9c
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2022-03-17 17:45:14 -0300

    validando dados existentes

--- core/serializers.py
+++ core/serializers.py
@@ -7,19 +7,43 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['id', 'name']
 
+    def validate(self, attrs):
+        category = Category.objects.filter(name=attrs['name']).first()
+        if category:
+            raise serializers.ValidationError(detail='Category with this name already existes!')
+        return super().validate(attrs)
 
-class QuizSerializer(serializers.ModelSerializer):
+class QuizSerializer(serializers.ModelSerializer):    
+    class Meta:
+        model = Quizzes
+        fields = ['id', 'title', 'category']
+    
+    def validate(self, attrs):
+        quiz = Quizzes.objects.filter(title=attrs['title']).first()
+        if quiz:
+            raise serializers.ValidationError(detail='Quiz with this title already existes!')
+        return super().validate(attrs)
+
+
+class ListQuizSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
     
     class Meta:
         model = Quizzes
         fields = ['id', 'title', 'category']
+        ready_only_fields = fields
 
 
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
         fields = ['id', 'question', 'answer_text', 'is_right']
+    
+    def validate(self, attrs):
+        answer = Answer.objects.filter(answer_text=attrs['answer_text']).first()
+        if answer:
+            raise serializers.ValidationError(detail='Answer already existes!')
+        return super().validate(attrs)
 
 
 class ListAnswerSerializer(serializers.ModelSerializer):
@@ -32,6 +56,12 @@ class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ['id', 'quiz', 'title']
+    
+    def validate(self, attrs):
+        question = Question.objects.filter(title=attrs['title']).first()
+        if question:
+            raise serializers.ValidationError(detail='Quiz with this title already existes!')
+        return super().validate(attrs)
 
 
 class ListQuestionSerializer(serializers.ModelSerializer):
